4444ab7d1ccff3efba4af2b912e2d65db66023fa
SergSivi <73647044+sergpalich@users.noreply.github.com>
2024-06-30 02:25:30 +0200

    Tercer ejerc: reponsive, flecha to cambiar pase, tres pases disponibles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
-import Card from './card';
+import Card1 from './card';
 import {useState} from 'react';
+import time_management from "../src/assets/time_managment.svg"
+import programming from "../src/assets/programming.svg"
+import meditation from "../src/assets/meditation.svg"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+
 
 function App() {
 
@@ -9,36 +17,37 @@ function App() {
     {
     title: "Dedica moltes hores",
     description: "Un mimim de 30 hores a la setmana. Si no en tens prou, haurias de dicar-li mes hores. Al principi semba impossible, pero notaras una millora rapidament ",
-    bgColor: " ",
-    image: " ",
+    bgColor: "#088A85",
+    image: time_management,
   
   },
 
   {
     title: "Programa projectes propis",
     description: "Mes val 10 hores treballant en projectes propis que 10 hores mirant tutorials.La motivacion i la implicacio en el projecte ajudara a accelerar el teu aprenntatge ",
-    bgColor: " ",
-    image: " ",
+    bgColor: "#D8D8D8",
+    image: programming,
   
   },
 
   {
     title: "Procura descansar",
     description: "Descansar be i desconnectar son vitals. D áquesta manera reduiras l estres i l ansietat. Milloras la teva concentracio i consolidaras el teu aprentage",
-    bgColor: " ",
-    image: " ",
-  
+    bgColor: "#F5DA81",
+    image: meditation,
   }
 ] 
   const currentCardData = tutorialData[step]
 
   function nextStep() {
-    setStep (prev => prev+1)
+    setStep (prev => (prev+1)%tutorialData.length)
   }
 
+
+  
   return (
     <div>
-      <Card currentCardData= {currentCardData}  nextStep={nextStep} />  
+      <Card1 currentCardData= {currentCardData}  nextStep={nextStep} />  
     </div>
   );
 };
--- src/card.js
+++ src/card.js
@@ -1,13 +1,30 @@
-const Card = (props) => {
-    const currentCardData = props.currentCardData;
-    const nextStep = props.nextStep;
+import { Card, Button, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App.css';
+
+
+
+
+const Card1 = (props) => {
+    const { currentCardData, nextStep } = props;
 
     return (
-    <div>
-    <h1>{currentCardData.title} </h1>
-    <p>{currentCardData.description} </p> 
-    <button onClick={nextStep}>Next step</button>
-    </div>
+        
+        <Container className="card text-center align-items-center col-10 col-sm-6 mt-4 mx-auto border-0">
+            <Card style={{ width: '18rem' } }>
+            <Card.Img  src={currentCardData.image} className="img-fluid" style={{backgroundColor: currentCardData.bgColor}}  />
+            <Card.Body>
+                <Card.Title> {currentCardData.title} </Card.Title>
+                <Card.Text>
+                {currentCardData.description}
+                </Card.Text>
+                <div className='d-flex justify-content-end'>
+                <Button className="button btn-circle bi bi-arrow-right" style={{backgroundColor: "#000000" , borderRadius: "50%"}} onClick={nextStep} ></Button>
+                </div>
+            </Card.Body>
+            </Card>
+        </Container>
 )};
  
-export default Card;
+export default Card1;
